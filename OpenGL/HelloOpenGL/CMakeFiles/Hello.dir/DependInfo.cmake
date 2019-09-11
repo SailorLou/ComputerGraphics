7afb529e6358186d187a074357e4dddef0fda2d7
sailorlou <loufuyi@13.com>
2019-09-11 23:10:38 +0800

    hello opengl

---
+++ OpenGL/HelloOpenGL/CMakeFiles/Hello.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sailor/works/ComputerGraphics/glad/src/glad.c" "/home/sailor/works/ComputerGraphics/OpenGL/HelloOpenGL/CMakeFiles/Hello.dir/home/sailor/works/ComputerGraphics/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/lib"
+  "../../glad/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sailor/works/ComputerGraphics/OpenGL/HelloOpenGL/helloOpenGL.cpp" "/home/sailor/works/ComputerGraphics/OpenGL/HelloOpenGL/CMakeFiles/Hello.dir/helloOpenGL.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/lib"
+  "../../glad/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenGL/HelloOpenGL/CMakeFiles/Hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello.dir/helloOpenGL.cpp.o"
+  "CMakeFiles/Hello.dir/home/sailor/works/ComputerGraphics/glad/src/glad.c.o"
+  "Hello.pdb"
+  "Hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
