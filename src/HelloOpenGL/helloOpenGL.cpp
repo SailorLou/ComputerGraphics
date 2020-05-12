1c0f25840894103b2433f0fbc53e7d00d247536a
615 <615@avic.com>
2020-05-12 19:56:16 +0800

    ccc

--- src/HelloOpenGL/helloOpenGL.cpp
+++ src/HelloOpenGL/helloOpenGL.cpp
@@ -17,6 +17,8 @@ int main()
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//告诉GLFW我们使用的是核心模式(Core-profile)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
@@ -33,6 +35,7 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
+	//每当窗口改变大小，GLFW会调用这个函数并填充相应的参数供你处理。
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
@@ -53,12 +56,22 @@ int main()
 
         // render
         // ------
+        // 在每个新的渲染迭代开始的时候我们总是希望清屏，
+        // 否则我们仍能看见上一次迭代的渲染结果（这可能是你想要的效果，
+        // 但通常这不是）
+        // 
+		//glClearColor函数是一个状态设置函数，
+		//而glClear函数则是一个状态使用的函数，
+		// 它使用了当前的状态来获取应该清除为的颜色
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
+
+		// glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、
+		// 更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）。
         glfwPollEvents();
     }
 
@@ -80,6 +93,13 @@ void processInput(GLFWwindow *window)
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+	//OpenGL幕后使用glViewport中定义的位置和宽高进行2D坐标的转换，
+	// //将OpenGL中的位置坐标转换为你的屏幕坐标。
+	// 例如，OpenGL中的坐标(-0.5, 0.5)有可能（最终）
+	// 被映射为屏幕中的坐标(200, 450)。
+	// 注意，处理过的OpenGL坐标范围只为 - 1到1，
+	// 因此我们事实上将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)。
+	//glViewport函数前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度（像素）
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
